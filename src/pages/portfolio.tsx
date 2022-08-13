a343eadf8ef6c287435d75640d524d0619547662
tatsuya19881021 <tatsuya19881021@gmail.com>
2022-08-13 22:11:21 +0900

    :sparkles: Portfolioページの作成

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,7 +1,52 @@
-import type { NextPage } from "next";
+import {
+  Box,
+  Divider,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import type { CustomNextPage } from "next";
+import { Layout } from "src/layout";
 
-const Portfolio: NextPage = () => {
-  return <div>Portfolioページ</div>;
+const articles = [...Array(6)].map(() => {
+  return {
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2022/08/09/22/06/field-7375948_960_720.jpg",
+    title: "IT KINGDOM",
+    description:
+      "当サロンのLPページ。React、Next.js、TypeScriptなどのモダンな技術を用いて作られています。初心者にちょうど良い難易度の制作物です。",
+    creationTime: "2021.10 - 2021.12",
+  };
+});
+
+const Portfolio: CustomNextPage = () => {
+  return (
+    <Box component="main">
+      <Stack spacing="lg">
+        <Title order={1}>Portfolio</Title>
+        <Divider />
+        <SimpleGrid cols={3} spacing="lg">
+          {articles.map((article, index) => (
+            <Stack key={index} spacing="lg">
+              <Image
+                width={316}
+                height={184}
+                src={article.thumbnail}
+                alt="Home Page Image"
+              />
+              <Title order={2}>{article.title}</Title>
+              <Text>{article.description}</Text>
+              <Text>{article.creationTime}</Text>
+            </Stack>
+          ))}
+        </SimpleGrid>
+      </Stack>
+    </Box>
+  );
 };
+
+Portfolio.getLayout = Layout;
 
 export default Portfolio;
