af631befd276c69da09269f025d178669e1cf612
tatsuya19881021 <tatsuya19881021@gmail.com>
2022-08-13 21:47:09 +0900

    :sparkles: Blogページの作成

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,44 @@
-import type { NextPage } from "next";
+import {
+  Box,
+  Center,
+  Divider,
+  Loader,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import type { CustomNextPage } from "next";
+import { Layout } from "src/layout";
 
-const Blog: NextPage = () => {
-  return <div>Blogページ</div>;
+const articles = [...Array(10)].map(() => {
+  return {
+    header: "This is a header",
+    body: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. ",
+    date: "2022.07.11",
+  };
+});
+
+const Blog: CustomNextPage = () => {
+  return (
+    <Box component="main">
+      <Stack spacing="lg">
+        <Title order={1}>Blog</Title>
+        <Divider />
+        {articles.map((article, index) => (
+          <Box key={index}>
+            <Title order={2}>{article.header}</Title>
+            <Text>{article.body}</Text>
+            <Text>{article.date}</Text>
+          </Box>
+        ))}
+        <Center>
+          <Loader color="pink.6" />
+        </Center>
+      </Stack>
+    </Box>
+  );
 };
+
+Blog.getLayout = Layout;
 
 export default Blog;
